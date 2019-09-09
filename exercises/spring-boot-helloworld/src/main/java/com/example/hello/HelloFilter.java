a0ed6e7ab1d675caafbbf1f067e198ce31bac40f
srikanthnagidi <srikanth.nagidi@gmail.com>
2019-09-09 18:22:20 -0500

    first spring boot program

---
+++ exercises/spring-boot-helloworld/src/main/java/com/example/hello/HelloFilter.java
@@ -0,0 +1,16 @@
+package com.example.hello;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(filterName = "helloFilter", urlPatterns = "/helloServlet")
+
+public class HelloFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)throws IOException, ServletException {
+        System.out.println("Executing doFilter Method");
+        filterChain.doFilter(servletRequest, servletResponse);
+        System.out.println("Done Executing doFilter Method");
+    }
+}
--- exercises/spring-boot-helloworld/src/main/java/com/example/springboothelloworld/HelloController.java
+++ exercises/spring-boot-helloworld/src/main/java/com/example/springboothelloworld/HelloController.java
@@ -1,18 +1,16 @@
 package com.example.springboothelloworld;
 
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
+@RestController
 public class HelloController {
     private Map<String, Object> result = new HashMap<>();
     @RequestMapping("/hello")
-    @ResponseBody
     public Map<String, Object> hello(){
         result.put("name", "Srikanth");
         result.put("city", "DeKalb");
