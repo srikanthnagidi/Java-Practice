46f2cb9cd468dbf23b4c94ff94a21b7977e87897
srikanthnagidi <srikanth.nagidi@gmail.com>
2019-09-11 15:54:06 -0500

    first servlet project

--- exercises/spring-boot-file-upload/src/main/java/com/example/springbootfileupload/controller/UploadController.java
+++ exercises/spring-boot-file-upload/src/main/java/com/example/springbootfileupload/controller/UploadController.java
@@ -11,16 +11,14 @@ import java.util.Map;
 
 public class UploadController {
     Map<String, Object> result = new HashMap<>();
-    /// Receive message
+    //Receive Message
     @RequestMapping("/uploadFile")
-    public Map<String, Object> upload(@RequestParam("attach") MultipartFile file) throws IOException {
-        // File info
-        System.out.println("File name = "  + file.getOriginalFilename());
-        System.out.println("File type = " + file.getContentType());
-
-        // Save to disk
-        // file path example 1) Windows c:/, 3) Mac ~/Documents/
-        String filePath = "~/Documents/";
+    public Map<String, Object> upload (@RequestParam("attach")MultipartFile file) throws IOException{
+        System.out.println("file name = " + file.getOriginalFilename());
+        System.out.println("file type = " + file.getContentType());
+        //save to disk
+        // file path example
+        String filePath = "~/Documents";
         file.transferTo(new File(filePath + file.getOriginalFilename()));
         result.put("Success", true);
         return result;
