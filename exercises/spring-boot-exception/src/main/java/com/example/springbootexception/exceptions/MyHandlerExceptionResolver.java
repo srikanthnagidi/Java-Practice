3dcb0396d3c1ca5818328cb5dcc884871ba0fe93
srikanthnagidi <srikanth.nagidi@gmail.com>
2019-10-03 20:18:03 -0500

    exception Handling in SpringBoot

---
+++ exercises/spring-boot-exception/src/main/java/com/example/springbootexception/exceptions/MyHandlerExceptionResolver.java
@@ -0,0 +1,25 @@
+package com.example.springbootexception.exceptions;
+
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyHandlerExceptionResolver implements HandlerExceptionResolver {
+
+    @Override
+    public ModelAndView resolveException(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, @Nullable Object o, Exception e) {
+        ModelAndView modelAndView = new ModelAndView();
+
+        if (e instanceof ArithmeticException){
+            modelAndView.setViewName("mathError");
+        }
+        if (e instanceof NullPointerException){
+            modelAndView.setViewName("nullPointerError");
+        }
+        modelAndView.addObject("exception", e.toString());
+        return modelAndView;
+    }
+}
