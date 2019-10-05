88057420c48ab4260cc18d70f68ddd976003e74f
srikanthnagidi <srikanth.nagidi@gmail.com>
2019-10-05 12:53:12 -0500

    web-socket

---
+++ exercises/spring-boot-wesocket/src/main/java/com/example/springbootwesocket/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.example.springbootwesocket.controller;
+
+import com.example.springbootwesocket.model.User;
+import com.example.springbootwesocket.model.UserResponse;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+
+public class UserController {
+
+    @MessageMapping("/user")
+    @SendTo("/topic/user")
+    public UserResponse getUser(User user){
+        return new UserResponse("Hello "+user.getName());
+    }
+}
