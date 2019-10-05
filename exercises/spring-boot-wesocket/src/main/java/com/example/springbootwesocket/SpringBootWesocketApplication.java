7b176572454e9146e4c9b0c7f932c6b9735c8309
srikanthnagidi <srikanth.nagidi@gmail.com>
2019-10-05 11:54:24 -0500

    web-socket

---
+++ exercises/spring-boot-wesocket/src/main/java/com/example/springbootwesocket/SpringBootWesocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootwesocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWesocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWesocketApplication.class, args);
+	}
+
+}
