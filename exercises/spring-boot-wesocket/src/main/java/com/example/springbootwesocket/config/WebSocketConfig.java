3bc7a36aea9fe9e36d3433e53bb49d511646b3ba
srikanthnagidi <srikanth.nagidi@gmail.com>
2019-10-06 17:34:40 -0500

    web-socket

--- exercises/spring-boot-wesocket/src/main/java/com/example/springbootwesocket/config/WebSocketConfig.java
+++ exercises/spring-boot-wesocket/src/main/java/com/example/springbootwesocket/config/WebSocketConfig.java
@@ -3,13 +3,13 @@ package com.example.springbootwesocket.config;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer{
+public class WebSocketConfig implements  WebSocketMessageBrokerConfigurer{
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config){
@@ -19,6 +19,7 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer{
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
+        registry.addEndpoint("/websocket-demo");
         registry.addEndpoint("/websocket-demo").withSockJS();
     }
 }
--- exercises/spring-boot-wesocket/src/main/java/com/example/springbootwesocket/controller/UserController.java
+++ exercises/spring-boot-wesocket/src/main/java/com/example/springbootwesocket/controller/UserController.java
@@ -4,12 +4,13 @@ import com.example.springbootwesocket.model.User;
 import com.example.springbootwesocket.model.UserResponse;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.util.HtmlUtils;
 
 public class UserController {
 
     @MessageMapping("/user")
     @SendTo("/topic/user")
     public UserResponse getUser(User user){
-        return new UserResponse("Hello "+user.getName());
+        return new UserResponse(HtmlUtils.htmlEscape("Hello "+user.getName()));
     }
 }
