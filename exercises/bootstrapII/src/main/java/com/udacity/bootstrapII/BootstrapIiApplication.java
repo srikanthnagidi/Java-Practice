e952fa3cdfd8dcd1774d052bced78075fd53cd7d
srikanthnagidi <srikanth.nagidi@gmail.com>
2019-10-13 19:12:31 -0500

    Rest Controller Practice

---
+++ exercises/bootstrapII/src/main/java/com/udacity/bootstrapII/BootstrapIiApplication.java
@@ -0,0 +1,13 @@
+package com.udacity.bootstrapII;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapIiApplication.class, args);
+	}
+
+}
