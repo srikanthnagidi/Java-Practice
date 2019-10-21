99b1871daf8c1b363984acb2cc8fe3e741ad5ca7
Srikanth Reddy Nagidi <srikanth.nagidi@gmail.com>
2019-10-21 13:54:52 -0500

    Dog example using graphql

---
+++ exercises/DOgGraphQL/src/main/java/com/example/DOgGraphQL/DOgGraphQlApplication.java
@@ -0,0 +1,13 @@
+package com.example.DOgGraphQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DOgGraphQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DOgGraphQlApplication.class, args);
+	}
+
+}
---
+++ exercises/DOgGraphQL/src/test/java/com/example/DOgGraphQL/DOgGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DOgGraphQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DOgGraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
