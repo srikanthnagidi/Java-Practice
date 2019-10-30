dab2f3546041eb728db894267db618d2e88602a8
Srikanth Reddy Nagidi <srikanth.nagidi@gmail.com>
2019-10-29 19:27:28 -0500

    added location controller unit test

---
+++ Web Services and APIS/bootstrap/src/test/java/com/udacity/bootstrap/web/LocationControllerUnitTest.java
@@ -0,0 +1,40 @@
+package com.udacity.bootstrap.web;
+
+import com.udacity.bootstrap.service.LocationService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(LocationController.class)
+public class LocationControllerUnitTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    LocationService locationService;
+
+    @WithMockUser(value = "admin")
+    @Test
+    public void getAllLocations() throws Exception{
+        mockMvc.perform(get("http://localhost:8080/locations"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(content().json("[]"));
+        //verify(locationService, times(1)).retrieveLocations();
+    }
+}
